8c07bae87acb391fd084ca4d593ce684c060dcef
Ravindra R. Jaju <jaju@msync.org>
2013-01-13 22:07:49 +0530

    [Travis] More trials - this time, fixing project.clj to get travis working...

--- project.clj
+++ project.clj
@@ -6,7 +6,12 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.5-alpha5" :exclusions [org.clojure/clojure]]]
 
-  :dev-dependencies [[lein-midje "3.0-alpha2"]]
+  :profiles {
+             :dev {
+                   :dependencies [[midje "1.5-alpha5" :exclusions [org.clojure/clojure]]]
+                   :plugins [[lein-midje "2.0.4"]]
+                 }
+             }
 
   :warn-on-reflection true
   )
