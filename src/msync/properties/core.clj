97c294dfb00ac380d147bcc643804395ee69200f
Ravindra R. Jaju <jaju@msync.org>
2013-09-23 22:19:14 +0530

    Added support for reading edn config, and replacing environment-var place-holders in the configuration maps.

--- src/msync/properties/core.clj
+++ src/msync/properties/core.clj
@@ -2,7 +2,9 @@
   (:import java.util.Properties
            [java.io StringBufferInputStream FileNotFoundException])
   (:require [clojure.string :refer [split join trim]]
-            [clojure.java.io :refer [reader]]))
+            [clojure.java.io :refer [reader]]
+            [clojure.edn :as edn]
+            [clojure.walk :refer [postwalk]]))
 
 ;;; Util
 (defn- key->path
@@ -25,42 +27,58 @@
           {} (keys props)))
 
 
-(defn- dump-config
+(defn- dump-properties
   "Dump a config map to a sequence of key-value pairs collapsing
    all nested keys if any."
   [props]
   (reduce-kv (fn [r k v] (if (map? v)
                           (let [k1 (name k)]
                             (concat (map (fn [[k2 v2]] [(str k1 "." (name k2)) v2])
-                                         (dump-config v))
+                                         (dump-properties v))
                                     r))
                           (conj r [(name k) v])))
              []
              props))
 
 
+(defn- getenv []
+  (System/getenv))
+
+(defn- ^:testable rewrite-placeholder-string [s smap]
+  (clojure.string/replace s #"\$(\w+)" (fn [[k v]] (get smap v "NONE"))))
+
+(defn- ^:testable rewrite-from-env [in-map smap]
+  (postwalk (fn [v] (if (string? v) (rewrite-placeholder-string v smap) v)) in-map))
+
 ;;; API
-(defn read-config
-  "Read a Java properties file into a map."
+(defn read-properties
+  "Read a Java properties file into a map. Replaces $ENVVAR placeholders from the environment."
   ([readable & {:keys [default nest-keys?] :or {nest-keys? true}}]
      (try
        (let [keyfn (or (and nest-keys? key->path) (comp vector keyword))
              props (load-props readable)]
-         (fold-props props keyfn))
+         (rewrite-from-env (fold-props props keyfn) (getenv)))
        (catch FileNotFoundException e
          (or default (throw e))))))
 
 
-(defn read-config-str
+(defn- ^:testable read-properties-str
   "Read a Java properties string into a map."
   [prop-str & {:keys [default nest-keys?] :or {nest-keys? true}}]
-  (read-config (StringBufferInputStream. prop-str) :default default :nest-keys? nest-keys?))
+  (read-properties (StringBufferInputStream. prop-str) :default default :nest-keys? nest-keys?))
 
 
-(defn write-config
+(defn- ^:testable write-properties
   "Dump a (nested) property map to Java properties format."
   [props]
-  (->> (dump-config props)
+  (->> (dump-properties props)
        (sort-by first)
        (map (fn [[k v]] (str k " = " v)))
        (join "\n")))
+
+(defn read-edn
+  "Reads an EDN file into a map. Duh! Oh yes - replaces $ENVVAR placeholders from the environment."
+  ([readable]
+     (rewrite-from-env (edn/read-string (slurp readable)) (getenv))))
+
+
--- test/msync/properties/core_test.clj
+++ test/msync/properties/core_test.clj
@@ -1,38 +1,40 @@
 (ns msync.properties.core-test
-  (:use midje.sweet
-        msync.properties.core)
+  (:require [midje.sweet :refer :all]
+            [midje.util :refer [expose-testables]]
+            [msync.properties.core :refer :all])
   (:import [java.io FileNotFoundException StringBufferInputStream]))
 
+(expose-testables msync.properties.core)
 
 (fact "Read a properties file and convert into a map with keys converted to clojure-keywords."
   (let [input-str "a.A=Y\nb=Z"]
-      (map (read-config-str input-str) (sorted-set :a :b)) => '({:A "Y"} "Z")
+      (map (read-properties-str input-str) (sorted-set :a :b)) => '({:A "Y"} "Z")
       ))
 
 (fact "Sub-configs from a config-map given a prefix. Note that these configs are just associative maps."
   (let [input-str "some-prefix.a = A\nsome-prefix.b = B"]
-      (-> input-str read-config-str :some-prefix) => {:a "A" :b "B"}))
+      (-> input-str read-properties-str :some-prefix) => {:a "A" :b "B"}))
 
 (fact "Some more extensive checks for sub-configs."
       (let [input-str "prefix1.prefix2.a = ABC
                        prefix1.prefix2.b = XYZ
                        prefix2.prefix1.Z = FOO"]
-        (-> input-str read-config-str :prefix1 :prefix2) => {:a "ABC" :b "XYZ"}
-        (-> input-str read-config-str :prefix2 :prefix1) => {:Z "FOO"}
-        (-> input-str (read-config-str :nest-keys? false)) => {:prefix1.prefix2.a "ABC" :prefix1.prefix2.b "XYZ" :prefix2.prefix1.Z "FOO"}))
+        (-> input-str read-properties-str :prefix1 :prefix2) => {:a "ABC" :b "XYZ"}
+        (-> input-str read-properties-str :prefix2 :prefix1) => {:Z "FOO"}
+        (-> input-str (read-properties-str :nest-keys? false)) => {:prefix1.prefix2.a "ABC" :prefix1.prefix2.b "XYZ" :prefix2.prefix1.Z "FOO"}))
 
 (fact "When a file is not found, and a default return is supplied, return it.
       Otherwise, bubble up the exception."
       (let [default-map {:a :A :b "B"}]
-        (read-config "non-existent-file.txt" :default default-map) => default-map
-        (read-config "non-existent-file.txt") => (throws FileNotFoundException)
+        (read-properties "non-existent-file.txt" :default default-map) => default-map
+        (read-properties "non-existent-file.txt") => (throws FileNotFoundException)
         (provided
           (#'msync.properties.core/load-props "non-existent-file.txt") =throws=> (FileNotFoundException.)))
       )
 
 (fact "Trims spaces around the values - effectively ignoring them"
       (let [input-str "a = A \n b = B"]
-        (read-config-str input-str) => {:a "A" :b "B"}))
+        (read-properties-str input-str) => {:a "A" :b "B"}))
 
 (fact "Creates nested maps-of-maps for hierarchical configuration data"
       (let [input-str "a = A
@@ -49,7 +51,7 @@
                  :d {:c {:a "DCA"
                          :b "DCB"
                          :e {:f "DCEF"}}}}]
-        (read-config-str input-str) => (just res)))
+        (read-properties-str input-str) => (just res)))
 
 (fact "Creates a properties map from string, and checks for correctness after round-trip."
       (let [input-str "a = A
@@ -66,22 +68,65 @@
                  :d {:c {:a "DCA"
                          :b "DCB"
                          :e {:f "DCEF"}}}}]
-        (read-config-str input-str) => (just res)
-        (read-config-str (write-config (read-config-str input-str))) => (just res)))
+        (read-properties-str input-str) => (just res)
+        (read-properties-str (write-properties (read-properties-str input-str))) => (just res)))
 
 (fact "Returns a sorted properties-format string from a map"
-  (write-config {:a "A" :b {:c "d"} :x {:y {:z "FOO"}}}) => "a = A\nb.c = d\nx.y.z = FOO")
+  (write-properties {:a "A" :b {:c "d"} :x {:y {:z "FOO"}}}) => "a = A\nb.c = d\nx.y.z = FOO")
+
+(def env-for-test {"HOME" "/home/jaju"
+                   "APPDIR" "/opt/appdir"
+                   "SECRETFILE" "secrets.key"
+                   "USER" "some_random_user"})
 
 (fact "Reads properties from a supplied file on disk."
-  (read-config "test/resources/test.properties") =>
+  (read-properties "test/resources/test.properties") =>
+  {
+   :name "configuration-clj"
+   :version "0.2.4"
+   :db {
+        :name "some_random_user-whacko-db"
+        :host "example.org"
+        :port "4567"}
+  :ring {
+         :handler {
+                   :ns "some-ring-handler-ns"
+                   :protocol "binary"}}}
+  (provided (#'msync.properties.core/getenv) => env-for-test))
+
+(fact "Reads EDN from a supplied file on disk."
+  (read-edn "test/resources/test-properties.edn") =>
   {
    :name "configuration-clj"
    :version "0.2.4"
    :db {
         :name "whacko-db"
         :host "example.org"
-        :port "4567"}
+        :port 4567
+        }
   :ring {
          :handler {
                    :ns "some-ring-handler-ns"
-                   :protocol "binary"}}})
+                   :protocol "binary"
+                   }}})
+
+
+(fact "Replaces $KEY place-holders in strings from values in the supplied map"
+  (let [env env-for-test]
+    (rewrite-placeholder-string "$HOME/abc" env) => "/home/jaju/abc"
+    (rewrite-placeholder-string "$APPDIR/config/$SECRETFILE" env) => "/opt/appdir/config/secrets.key"))
+
+(fact "Replaces $KEY place-holders in string values of the input map from values in supplied map."
+  (let [env env-for-test
+        config-map {
+                    :file "$HOME/configuration-clj"
+                    :db {
+                         :data-dir "$APPDIR/whacko-db"
+                         }
+                    :password "$HOME/config/$SECRETFILE"}]
+    (rewrite-from-env config-map env) => {
+                    :file "/home/jaju/configuration-clj"
+                    :db {
+                         :data-dir "/opt/appdir/whacko-db"
+                         }
+                    :password "/home/jaju/config/secrets.key"}))
