62c697a09d7bf56f737779d02795a187767334c6
Ravindra R. Jaju <jaju@msync.org>
2014-10-29 15:26:10 +0530

    Updates: clojure and midje versions.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject org.msync/properties-clj "0.3.0"
+(defproject org.msync/properties-clj "0.3.1"
   :description "A simple wrapper for properties files with useful hierarchical keys based property-maps"
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.5.1" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [midje "1.6.3" :exclusions [org.clojure/clojure]]]
 
   :profiles {
              :dev {
-                   :dependencies [[midje "1.5.1" :exclusions [org.clojure/clojure]]]
-                   :plugins [[lein-midje "3.1.0"]]
+                   :dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]]
+                   :plugins [[lein-midje "3.1.3"]]
                    }
              }
   )
