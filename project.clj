84144aeafd3416e260d7ec64c4fa2a159e77c835
Ravindra R. Jaju <jaju@msync.org>
2015-05-21 19:23:39 +0530

    Changed version to a higher, SNAPSHOT one - with clojure 1.7.0-beta3 and midje 1.7.0-beta1

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject org.msync/properties-clj "0.3.1"
+(defproject org.msync/properties-clj "0.3.2-SNAPSHOT"
   :description "A simple wrapper for properties files with useful hierarchical keys based property-maps"
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [midje "1.6.3" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [midje "1.7.0-beta1" :exclusions [org.clojure/clojure]]]
 
   :profiles {
              :dev {
-                   :dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]]
+                   :dependencies [[midje "1.7.0-beta1" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "3.1.3"]]
                    }
              }
