5c454add09524ee4af00a4adc4b1657d76b547e3
Ravindra R. Jaju <jaju@msync.org>
2016-02-18 15:53:17 +0530

    0.3.3-SNAPSHOT - clojure version updated.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.msync/properties-clj "0.3.2"
+(defproject org.msync/properties-clj "0.3.3-SNAPSHOT"
   :description "A simple wrapper for properties files with useful hierarchical keys based property-maps"
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [midje "1.7.0" :exclusions [org.clojure/clojure]]]
 
   :profiles {
@@ -12,4 +12,7 @@
                    :plugins [[lein-midje "3.1.3"]]
                    }
              }
+
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   )
